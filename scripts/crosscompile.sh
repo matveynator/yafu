f5b8d1c32b07dc45f9a0dbfd2d64459dda686622
Matvey Gladkikh <matvey@copters.ru>
2023-09-23 02:57:51 +0300

    name

--- scripts/crosscompile.sh
+++ scripts/crosscompile.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
+app_name="yafu"
 version="0.1-002"
 git_root_path=`git rev-parse --show-toplevel`
-execution_file="yafu"
 
+go mod init ${app_name}
 go mod download
 go mod vendor
 go mod tidy
+
+execution_file="${app_name}"
 
 echo "Performing tests on all modules..."
 go test ./...
@@ -32,12 +35,12 @@ do
 	for arch in "amd64" "386" "arm" "arm64" "mips64" "mips64le" "mips" "mipsle" "ppc64" "ppc64le" "riscv64" "s390x" "wasm"
 	do
 		target_os_name=${os}
-		[ "$os" == "windows" ] && execution_file="yafu.exe"
+		[ "$os" == "windows" ] && execution_file="${app_name}.exe"
 		[ "$os" == "darwin" ] && target_os_name="mac"
 		
 		mkdir -p ../binaries/${version}/${target_os_name}/${arch}
 
-		GOOS=${os} GOARCH=${arch} go build -ldflags "-X yafu/pkg/config.CompileVersion=${version}" -o ../binaries/${version}/${target_os_name}/${arch}/${execution_file} ../yafu.go 2> /dev/null
+		GOOS=${os} GOARCH=${arch} go build -ldflags "-X ${app_name}/pkg/config.CompileVersion=${version}" -o ../binaries/${version}/${target_os_name}/${arch}/${execution_file} ../${app_name}.go 2> /dev/null
 
 
 		if [ "$?" != "0" ]
@@ -45,7 +48,7 @@ do
 		then
 		  rm -rf ../binaries/${version}/${target_os_name}/${arch}
 		else
-		  echo "GOOS=${os} GOARCH=${arch} go build -ldflags "-X yafu/pkg/config.CompileVersion=${version}" -o ../binaries/${version}/${target_os_name}/${arch}/${execution_file} ../yafu.go"
+		  echo "GOOS=${os} GOARCH=${arch} go build -ldflags "-X ${app_name}/pkg/config.CompileVersion=${version}" -o ../binaries/${version}/${target_os_name}/${arch}/${execution_file} ../${app_name}.go"
       chmod +x ../binaries/${version}/${target_os_name}/${arch}/${execution_file}
       cp ../candidates.txt ../binaries/${version}/${target_os_name}/${arch}/
 		fi
@@ -53,4 +56,4 @@ do
 done
 
 #optional: publish to internet:
-rsync -avP ../binaries/* files@files.matveynator.ru:/home/files/public_html/yafu/
+rsync -avP --delete ../binaries/* files@files.matveynator.ru:/home/files/public_html/${app_name}/
